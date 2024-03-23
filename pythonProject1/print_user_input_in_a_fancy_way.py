fc0d4ccf4297eb5735c28b40707d9ce95521e447
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-03-23 21:57:23 +0800

    Pseudocode

---
+++ pythonProject1/print_user_input_in_a_fancy_way.py
@@ -0,0 +1,14 @@
+# pseudocode
+
+# ask the user to input name
+# ask the user to input age
+# validate age
+# ask the user to input dream job
+# add art to name
+# add art to age
+# add art to dream job
+# print name in a fancy way
+# print age in a fancy way
+# print dream job in a fancy way
+# add printing animation
+# add printing animation function
