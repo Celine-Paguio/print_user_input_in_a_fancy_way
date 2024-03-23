1afad6b2dd7422826e078a43d08857328a709d05
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-03-23 22:28:36 +0800

    Add art to input

--- pythonProject1/print_user_input_in_a_fancy_way.py
+++ pythonProject1/print_user_input_in_a_fancy_way.py
@@ -1,3 +1,5 @@
+from art import text2art
+
 # pseudocode
 
 # ask the user to input name
@@ -7,14 +9,17 @@ name = input("Please enter your name: ")
 while True:
     age = int(input("Please enter your age: "))
     if age <= 0 or age > 100:
-        print ("Invalid Input! Please enter a valid age: ")
+        print("Invalid Input! Please enter a valid age: ")
     else:
         break
 # ask the user to input dream job
 dream_job = input("Please enter your dream job: ")
 # add art to name
+art_name = text2art(name, font='block', chr_ignore=True)
 # add art to age
+art_age = text2art(str(age), font='block', chr_ignore=True)
 # add art to dream job
+art_dream_job = text2art(dream_job, font='block', chr_ignore=True)
 # print name in a fancy way
 # print age in a fancy way
 # print dream job in a fancy way
