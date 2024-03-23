8fe63d8920b0af85f111ecde20fb7dd936031a10
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-03-23 22:02:10 +0800

    Ask the user for input

--- pythonProject1/print_user_input_in_a_fancy_way.py
+++ pythonProject1/print_user_input_in_a_fancy_way.py
@@ -1,9 +1,12 @@
 # pseudocode
 
 # ask the user to input name
+name = input("Please enter your name: ")
 # ask the user to input age
-# validate age
+age = input("Please enter your age: ")
 # ask the user to input dream job
+dream_job = input("Please enter your dream job: ")
+# validate age
 # add art to name
 # add art to age
 # add art to dream job
