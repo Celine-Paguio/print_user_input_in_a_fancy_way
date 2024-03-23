eee919bdf0733a5177e88b9792e163f690ee0fca
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-03-23 22:56:14 +0800

    Add printing animation function

--- pythonProject1/print_user_input_in_a_fancy_way.py
+++ pythonProject1/print_user_input_in_a_fancy_way.py
@@ -4,7 +4,6 @@ import sys
 import time
 
 # pseudocode
-
 # ask the user to input name
 name = input(Fore.LIGHTYELLOW_EX + "Please enter your name: ")
 # ask the user to input age
@@ -23,26 +22,24 @@ art_name = text2art(name, font='block', chr_ignore=True)
 art_age = text2art(str(age), font='block', chr_ignore=True)
 # add art to dream job
 art_dream_job = text2art(dream_job, font='block', chr_ignore=True)
-# print name in a fancy way
-print(Fore.LIGHTMAGENTA_EX + text2art("Name: ", font='block', chr_ignore=True))
-print(Fore.LIGHTCYAN_EX + text2art(name, font='block', chr_ignore=True))
-# print age in a fancy way
-print(Fore.LIGHTMAGENTA_EX + text2art("Age: ", font='block'))
-print(Fore.LIGHTCYAN_EX + text2art(str(age), font='block', chr_ignore=True))
-# print dream job in a fancy way
-print(Fore.LIGHTMAGENTA_EX + text2art("Dream Job: ", font='block'))
-print(Fore.LIGHTCYAN_EX + text2art(dream_job, font='block', chr_ignore=True))
-# add printing animation
-for char in name:
-    sys.stdout.write(char)
-    sys.stdout.flush()
-    time.sleep(0.001)
-for char in str(age):
-    sys.stdout.write(char)
-    sys.stdout.flush()
-    time.sleep(0.001)
-for char in dream_job:
-    sys.stdout.write(char)
-    sys.stdout.flush()
-    time.sleep(0.001)
+
+
 # add printing animation function
+def printing(animation):
+    for text in animation:
+        sys.stdout.write(text)
+        sys.stdout.flush()
+        time.sleep(0.001)
+
+
+# print name in a fancy way
+printing(Fore.LIGHTMAGENTA_EX + text2art("Name: ", font='block', chr_ignore=True))
+printing((Fore.LIGHTCYAN_EX + text2art(name, font='block', chr_ignore=True)))
+# print age in a fancy way
+printing(Fore.LIGHTMAGENTA_EX + text2art("Age: ", font='block'))
+printing(Fore.LIGHTCYAN_EX + text2art(str(age), font='block', chr_ignore=True))
+# print dream job in a fancy way
+printing(Fore.LIGHTMAGENTA_EX + text2art("Dream Job: ", font='block'))
+printing(Fore.LIGHTCYAN_EX + text2art(dream_job, font='block', chr_ignore=True))
+
+# end of program
