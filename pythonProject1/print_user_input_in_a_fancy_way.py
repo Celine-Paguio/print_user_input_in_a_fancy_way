0b262cbe1ee6902ed20c30f67f661bb2737ff13a
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-03-23 22:32:42 +0800

    Add art to input (color)

--- pythonProject1/print_user_input_in_a_fancy_way.py
+++ pythonProject1/print_user_input_in_a_fancy_way.py
@@ -1,19 +1,20 @@
 from art import text2art
+from colorama import Fore
 
 # pseudocode
 
 # ask the user to input name
-name = input("Please enter your name: ")
+name = input(Fore.LIGHTYELLOW_EX + "Please enter your name: ")
 # ask the user to input age
 # validate age
 while True:
-    age = int(input("Please enter your age: "))
+    age = int(input(Fore.LIGHTYELLOW_EX + "Please enter your age: "))
     if age <= 0 or age > 100:
         print("Invalid Input! Please enter a valid age: ")
     else:
         break
 # ask the user to input dream job
-dream_job = input("Please enter your dream job: ")
+dream_job = input(Fore.LIGHTYELLOW_EX + "Please enter your dream job: ")
 # add art to name
 art_name = text2art(name, font='block', chr_ignore=True)
 # add art to age
